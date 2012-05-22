4a49aad0458ea8edf1a3c4f40c8d12b633b2b7a0
Simone Orsi <simahawk@gmail.com>
2012-05-22 17:39:43 +0200

    [imp] if cron4plone is present show info about registered cron if any

--- collective/socialpublisher/browser/viewlets.py
+++ collective/socialpublisher/browser/viewlets.py
@@ -4,6 +4,12 @@ from zope.component import getMultiAdapter
 from Products.CMFCore.utils import getToolByName
 from plone.memoize.view import memoize
 
+try:
+    import Products.cron4plone
+    HAS_CRON4PLONE = True
+except ImportError:
+    HAS_CRON4PLONE = False
+
 from collective.socialpublisher.interfaces import IAutoPublishable
 from collective.socialpublisher.interfaces import IPublishStorageManager
 from collective.socialpublisher.config import AUTOPUBLISHABLE_CONTENTTYPES
@@ -14,9 +20,6 @@ class Social(ViewletBase):
     
     render = ViewPageTemplateFile('viewlet_social.pt')
     
-    def update(self):
-        pass
-
     def available(self):
         return not self.context._at_creation_flag
         
@@ -60,3 +63,30 @@ class Social(ViewletBase):
         saved = manager.get_text()
         default = utils.get_text(self.context)
         return saved or default
+
+    @property
+    def show_cron_info(self):
+        return HAS_CRON4PLONE
+
+    @property
+    def cron(self):
+        return self._get_cron()
+
+    @memoize
+    def _get_cron(self):
+        cron = None
+        if HAS_CRON4PLONE:
+            tool = getToolByName(self.context,'CronTool',None)
+            if tool is not None:
+                schedule = None
+                crondata = tool._getCronData()
+                # [{'expression': u'portal/@@social-auto-publish', 
+                # 'id': 0, 'schedule': [u'*', u'*', u'*', u'*']}]
+                for item in crondata:
+                    if '@@social-auto-publish' in item.get('expression',''):
+                        schedule = item.get('schedule')
+                        break
+                if schedule is not None:
+                    legend = ('min','hour', 'day of month', 'month')
+                    cron = ', '.join([': '.join((k,v)) for k,v in  zip(legend,schedule)])
+        return cron
